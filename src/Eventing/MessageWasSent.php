c64807c426652f47501cf38831c854e9c144ec79
Karan Ganwani <karan.ganwani@atliq.com>
2021-05-31 11:51:31 +0530

    refactor: commented should broacast

--- src/Eventing/MessageWasSent.php
+++ src/Eventing/MessageWasSent.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Musonza\Chat\Models\Message;
 
-class MessageWasSent extends Event implements ShouldBroadcast
+class MessageWasSent extends Event //implements ShouldBroadcast
 {
     use Dispatchable;
     use InteractsWithSockets;
